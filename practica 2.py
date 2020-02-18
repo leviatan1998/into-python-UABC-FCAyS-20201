a30c3b33793aec1604bb513ba5c545b02fa3b570
Leviatan <61199644+leviatan1998@users.noreply.github.com>
2020-02-18 08:31:22 -0800

    Practica 2
    Practica con un ciclo de ejemplo, clase de Introducción a la Programación con Python

---
+++ practica 2.py
@@ -0,0 +1,8 @@
+# Esta practica nos sirve para entender ciclos
+n = input("Ingresa un numero entre 0 y 20") # variable n inicializada como 5
+n = int(n)
+
+while n > 0 :
+  print(n)
+  n = n - 1
+print("Final")

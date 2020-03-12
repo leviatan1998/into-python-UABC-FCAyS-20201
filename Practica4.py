b2f6b594f7523921b091d064bb3f2c12d86182ee
Leviatan <61199644+leviatan1998@users.noreply.github.com>
2020-03-12 09:00:53 -0700

    Nueva practica enfocada en diagramas de flujo
    La practica 4 fue un sencillo algoritmo para sumar números acompañado de su respectivo diagrama.

---
+++ Practica4.py
@@ -0,0 +1,9 @@
+# El siguiente algoritmo resulve el problema
+# para realizar la suma de dos numeros decimos
+a = 0
+b = 0
+c = 0
+a = float(input("Ingrese el primer numero:"))
+b = float(input("Ingrese el segundo numero:"))
+c = int(a) + int(b)
+print("La suma es:", c)

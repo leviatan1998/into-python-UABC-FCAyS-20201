5a148748ea60e4dacbde92fcb730937222f373e0
Leviatan <61199644+leviatan1998@users.noreply.github.com>
2020-03-12 09:44:21 -0700

    OperacionesDeDosNumeros

---
+++ Practica4.1.py
@@ -0,0 +1,12 @@
+# El siguiente algoritmo resulve el problema
+# para realizar la suma de dos numeros decimos
+a = 0
+b = 0
+c = 0
+a = float(input("Ingrese el primer numero:"))
+b = float(input("Ingrese el segundo numero:"))
+if a > 0 or b > 0 :
+  c = a + b
+else :
+  c = a * b
+print("La suma es:", c)
